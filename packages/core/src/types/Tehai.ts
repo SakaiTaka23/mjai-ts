ba2e87710a8e72391f7302a22f0a591ef4826d62
SakaiTaka23 <tt3.sakai@gmail.com>
2025-05-05 21:17:46 +0900

    defined tehai type

---
+++ packages/core/src/types/Tehai.ts
@@ -0,0 +1,9 @@
+import { Ankan, Chi, Daiminkan, Kakan, Pon, Tile } from "@mjai/types";
+
+export interface HandState {
+    tehai: Tile[]
+    tsumo: Tile | null
+    fuuros: Fuuro[]
+}
+
+type Fuuro = Ankan | Chi | Daiminkan | Kakan | Pon
