213e466982f7135548985dc115736a2fde48c67a
SakaiTaka23 <tt3.sakai@gmail.com>
2025-05-10 15:13:59 +0900

    connected with main state

--- packages/core/src/GameState.ts
+++ packages/core/src/GameState.ts
@@ -1,11 +1,17 @@
+import { Tile } from '@mjai/types';
+
 import { BaseState } from './BaseState';
 import { HandState } from './types/Tehai';
 
 export interface GameState {
   TehaiState: TehaiState;
+  DoraState: DoraState;
 }
 
 //  extend so that we can add more utility functions
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface TehaiState
   extends BaseState<[HandState, HandState, HandState, HandState]> {}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface DoraState extends BaseState<Tile[]> {}
--- packages/core/src/State.ts
+++ packages/core/src/State.ts
@@ -1,12 +1,15 @@
 import { StartKyoku } from '@mjai/types';
 
+import { DoraState } from './DoraState';
 import { GameState } from './GameState';
 import { TehaiState } from './TehaiState';
 
 export const createGameState = (start: StartKyoku): GameState => {
   const tehaiState = TehaiState(start);
+  const doraState = DoraState(start);
 
   return {
     TehaiState: tehaiState,
+    DoraState: doraState,
   };
 };
