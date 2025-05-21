2aa907560822c2fcd13290d95ff1e06393a1b198
SakaiTaka23 <tt3.sakai@gmail.com>
2025-05-22 04:08:21 +0900

    exported state types

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,10 @@
 export { createGameState } from './State';
+export type {
+  GameState,
+  TehaiState,
+  DoraState,
+  ScoreState,
+  KawaState,
+} from './GameState';
+export type { HandState } from './types/Tehai';
+export type { Kawa } from './types/Kawa';
