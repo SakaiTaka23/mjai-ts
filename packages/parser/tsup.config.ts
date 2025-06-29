9342bd409761121e796eb1f5e15546cb06e1a469
SakaiTaka23 <tt3.sakai@gmail.com>
2025-06-29 11:01:57 +0900

    implicity add typescript as dev dependency

--- packages/parser/tsup.config.ts
+++ packages/parser/tsup.config.ts
@@ -1,10 +1,10 @@
-import {defineConfig} from 'tsup';
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entry: ['src/index.ts'],
-    dts: true,
-    format: ['esm'],
-    outDir: 'dist',
-    clean: true,
-    treeshake: false,
+  entry: ['src/index.ts'],
+  dts: true,
+  format: ['esm'],
+  outDir: 'dist',
+  clean: true,
+  treeshake: false,
 });
