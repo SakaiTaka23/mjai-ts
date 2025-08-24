034c5ef96d5c21e60635c6fca7e9d7e319cb192d
SakaiTaka23 <tt3.sakai@gmail.com>
2025-08-24 20:28:07 +0900

    adjusted types

--- packages/types/tsup.config.ts
+++ packages/types/tsup.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  dts: true,
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   outDir: 'dist',
   clean: true,
 });
